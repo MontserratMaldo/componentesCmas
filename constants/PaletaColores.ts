11790c879545ac40cfbfb9e9074c8126d92da769
DianaElisa23 <zS23017370@estudiantes.uv.mx>
2025-11-10 12:14:55 -0600

    Add files via upload

---
+++ constants/PaletaColores.ts
@@ -0,0 +1,46 @@
+import { Platform } from 'react-native';
+
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+export const blueSomeDark = {
+  ligter: {
+    normal: { hex: "#8cceed" , rbg : "rbg(140, 206, 237)" },
+    hover: { hex: "#75c5ea" , rbg : "rbg(117, 197, 234)" },
+    active : {hex : "#59b9e6", rbg: "rbg(89, 185, 230)"}
+  }, 
+
+  normal: {
+    normal: {hex: "#0093d8" , rbg: "rbg(0, 147, 216)" },
+    hover: {hex: "#007db8", rbg: "rbg(0, 125, 184)" },
+    active: {hex: "#006b9e", rbg: "rbg(0, 107, 158)" }
+  }, 
+
+  dark: {
+    normal: {hex: "#00334c", rbg: "rbg(0, 51, 76)" },
+    hover: {hex: "#002536", rbg: "rbg(0, 37, 54)" },
+    active: {hex: "#001018", rbg: "rbg(0, 16, 24)" }
+  }
+
+}
+
+export const blue = {
+  ligth: {
+    normal : {hex: "#e6f4fb", rbg: "rbg(230, 244, 251)"},
+    hover: {hex: "#d9eff9", rbg: "rbg(217, 239, 249)"},
+    active: {hex: "#b0def3", rbg: "rbg(176, 222, 243)"}
+  }, 
+
+  normal: {
+    normal: {hex: "#0093d8", rbg: "rbg(0, 147, 216)" },
+    hover: {hex: "#0084c2", rbg: "rbg(0, 132, 194)" },
+    active: {hex: "#0076ad", rbg: "rbg(0, 118, 173)"}
+  }, 
+
+  dark: {
+    normal: {hex: "#006ea2", rbg: "rbg(0, 110, 162)" },
+    hover: {hex: "#005882", rbg: "rbg(0, 88, 130)" },
+    active: {hex: "#004261", rbg: "rbg(0, 66, 97)" },
+    darker: {hex: "#00334c", rbg: "rbg(0, 51, 76)" }
+  }
+}
---
+++ constants/Tipografia.ts
@@ -0,0 +1,65 @@
+export const Typography = {
+
+  fontFamily: {
+    bold: 'Poppins-Bold',
+    semiBold: 'Poppins-SemiBold',
+    medium: 'Poppins-Medium',
+    light: 'Poppins-Light'
+  },
+
+  h1: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 30,
+    lineHeight: 38,
+    fontWeight: '700',
+
+  },
+  h2: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 28,
+    lineHeight: 35,
+    fontWeight: '700',
+  },
+  h3: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 26,
+    lineHeight: 31,
+    fontWeight: '700',
+  },
+  h4: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 24,
+    lineHeight: 28,
+    fontWeight: '700',
+  },
+  h5: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 22,
+    lineHeight: 26,
+    fontWeight: '700',
+  },
+  title1: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 19,
+    lineHeight: 22,
+    fontWeight: '700',
+  },
+  title2: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 17,
+    lineHeight: 20,
+    fontWeight: '700',
+  },
+  text: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 15,
+    lineHeight: 20,
+    fontWeight: '700',
+  },
+  secondaryText: {
+    fontFamily: 'Poppins-Light',
+    fontSize: 12,
+    lineHeight: 18,
+    fontWeight: '700',
+  },
+};
